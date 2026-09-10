929fd2ac4c889b73bf259f5393e3647be07d8448
Adesina Yusuf <adexyuslove@gmail.com>
2026-09-09 21:33:40 -0700

    fix: sort logo issue

--- app/components/layouts/Navbar.tsx
+++ app/components/layouts/Navbar.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function () {
+const Navbar = () => {
     return (
-        <div className="flex items-center justify-between p-4 bg-white text-black  ">
-            <div className="">
-                <Image src="/logo.svg" alt="Logo" width={50} height={50} />
+        <div className="flex items-center justify-between bg-background p-4 text-secondary">
+            <div className="max-w-56 max-h-20.75">
+                <Image src="images/logo.svg" alt="Logo" width={0} height={0} sizes='100vw' style={{ width: '100%', height: 'auto' }} />
             </div>
 
             <div className="flex gap-5">
@@ -18,3 +18,5 @@ export default function () {
         </div>
     )
 }
+
+export default Navbar;
