094bfd49f487a385d34d872afb77d9b4bf7bafd2
Bxr <olanrewajuhussain019@gmail.com>
2026-09-11 23:56:13 +0100

    feat: add Banner component and integrate into About and Services pages

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Banner from '../components/Banner'
+
+
 
 export default function Services() {
   return (
-    <div>Welcome to services page</div> 
+    <>
+      <Banner bannerTitle="Services" bannerDescription="From software development and digital transformation to document 
+management and GIS, we bring the right technology to every challenge." />
+    </>
+
   )
 }
