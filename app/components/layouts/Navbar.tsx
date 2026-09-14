e546f53c1c1284024a714c6df0eb9798e81cdb0b
Etinosa22 <info@geospatial.ng>
2026-09-14 17:12:05 +0100

    fix: reorder contact link in Navbar and add contact page component

--- app/components/layouts/Navbar.tsx
+++ app/components/layouts/Navbar.tsx
@@ -12,8 +12,8 @@ const Navbar = () => {
             <div className="flex gap-5">
                 <Link href="/">Home</Link>
                 <Link href="/about">About</Link>
-                <Link href="/contact">Contact</Link>
                 <Link href="/services">Services</Link>
+                <Link href="/contact">Contact</Link>
             </div>
         </div>
     )
